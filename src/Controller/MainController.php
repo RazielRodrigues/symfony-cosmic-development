a344dede0e2b3eb42e5018b908340944bc8e2b07
Raziel Rodrigues <razielx3@live.com>
2024-04-07 22:00:36 +0100

    feat: starting with twig

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,15 +2,18 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class MainController
+class MainController extends AbstractController
 {
 
     #[Route('/')]
     public function homepage(): Response
     {
-        return new Response('Hello');
+        return $this->render('main/index.html.twig', [
+            'name' => 'Raziel Rodrigues',
+        ]);
     }
 }
